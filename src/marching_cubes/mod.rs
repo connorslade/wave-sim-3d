03363f5a7223e8cb62431177dadf809b23a25b67
Connor Slade <connor@connorcode.com>
2025-03-23 21:31:16 -0400

    Cleanup marching cubes

--- src/marching_cubes/mod.rs
+++ src/marching_cubes/mod.rs
@@ -1,3 +1,4 @@
+/// Implementation of <https://paulbourke.net/geometry/polygonise>.
 use compute::{
     export::nalgebra::{Vector2, Vector3},
     pipeline::render::Vertex,
@@ -5,6 +6,24 @@ use compute::{
 use table::{EDGE_TABLE, TRIANGULATION_TABLE};
 
 mod table;
+
+#[rustfmt::skip]
+const EDGE_CONNECTIONS: [(usize, usize); 12] = [
+    (0, 1), (1, 2), (2, 3), (3, 0),
+    (4, 5), (5, 6), (6, 7), (7, 4),
+    (0, 4), (1, 5), (2, 6), (3, 7)
+];
+
+const GRID_POINTS: [Vector3<usize>; 8] = [
+    Vector3::new(0, 0, 0),
+    Vector3::new(1, 0, 0),
+    Vector3::new(1, 0, 1),
+    Vector3::new(0, 0, 1),
+    Vector3::new(0, 1, 0),
+    Vector3::new(1, 1, 0),
+    Vector3::new(1, 1, 1),
+    Vector3::new(0, 1, 1),
+];
 
 pub fn marching_cubes(
     scalar_field: &[f32],
@@ -16,74 +35,42 @@ pub fn marching_cubes(
     for x in 0..size.x - 1 {
         for y in 0..size.y - 1 {
             for z in 0..size.z - 1 {
+                let mut grid = [(Vector3::zeros(), 0.0); 8];
                 let mut cube_index = 0;
 
-                let mut grid = [(Vector3::zeros(), 0.0); 8];
-                let positions = [
-                    (x as f32, y as f32, z as f32),
-                    ((x + 1) as f32, y as f32, z as f32),
-                    ((x + 1) as f32, y as f32, (z + 1) as f32),
-                    (x as f32, y as f32, (z + 1) as f32),
-                    (x as f32, (y + 1) as f32, z as f32),
-                    ((x + 1) as f32, (y + 1) as f32, z as f32),
-                    ((x + 1) as f32, (y + 1) as f32, (z + 1) as f32),
-                    (x as f32, (y + 1) as f32, (z + 1) as f32),
-                ];
+                for (i, offset) in GRID_POINTS.iter().enumerate() {
+                    let pos = Vector3::new(x, y, z) + offset;
 
-                for (i, &(px, py, pz)) in positions.iter().enumerate() {
-                    let index =
-                        (px as usize) * size.y * size.z + (py as usize) * size.z + (pz as usize);
+                    let index = pos.x * size.y * size.z + pos.y * size.z + pos.z;
                     let value = scalar_field[index];
 
-                    grid[i] = (Vector3::new(px, py, pz), value);
-                    if value < iso_level {
-                        cube_index |= 1 << i;
-                    }
+                    grid[i] = (pos.map(|x| x as f32), value);
+                    cube_index |= ((value < iso_level) as usize) << i;
                 }
 
                 let edge = EDGE_TABLE[cube_index];
-
                 let mut vertlist = [Vector3::zeros(); 12];
-                for (i, &(p1, p2)) in [
-                    (0, 1),
-                    (1, 2),
-                    (2, 3),
-                    (3, 0),
-                    (4, 5),
-                    (5, 6),
-                    (6, 7),
-                    (7, 4),
-                    (0, 4),
-                    (1, 5),
-                    (2, 6),
-                    (3, 7),
-                ]
-                .iter()
-                .enumerate()
+                for (i, &(p1, p2)) in EDGE_CONNECTIONS
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| edge & (1 << i) != 0)
                 {
-                    if edge & (1 << i) != 0 {
-                        vertlist[i] = vertex_interp(iso_level, grid[p1], grid[p2]);
-                    }
+                    vertlist[i] = vertex_interp(iso_level, grid[p1], grid[p2]);
                 }
 
                 let triangles = TRIANGULATION_TABLE[cube_index];
                 for triangle in triangles.chunks(3) {
-                    vertices.push(Vertex::new(
-                        vertlist[triangle[0] as usize].push(1.0),
-                        Vector2::zeros(),
-                    ));
-                    vertices.push(Vertex::new(
-                        vertlist[triangle[1] as usize].push(1.0),
-                        Vector2::zeros(),
-                    ));
-                    vertices.push(Vertex::new(
-                        vertlist[triangle[2] as usize].push(1.0),
-                        Vector2::zeros(),
-                    ));
+                    for &vert in triangle {
+                        vertices.push(Vertex::new(
+                            vertlist[vert as usize].push(1.0),
+                            Vector2::zeros(),
+                        ));
+                    }
+
                     indices.extend_from_slice(&[
-                        (vertices.len() - 1) as u32,
-                        (vertices.len() - 2) as u32,
                         (vertices.len() - 3) as u32,
+                        (vertices.len() - 2) as u32,
+                        (vertices.len() - 1) as u32,
                     ]);
                 }
             }
